8ca33efd22dc07e39d8aaa33b469887957129ef8
Arthur Dufour <arthur@road-b-score.com>
2021-05-17 17:29:22 +0200

    refactor(login): option to show steps

--- pages/login.vue
+++ pages/login.vue
@@ -1,5 +1,24 @@
 <template>
-  <LoginForm />
+  <div>
+    <div class="w-full max-w-3xl mt-4 mx-auto flex flex-shrink-0">
+      <!-- form -->
+      <LoginForm />
+
+      <!-- steps -->
+      <div v-show="$route.query.showSteps" class="pt-12 pl-8 hidden md:block">
+        <div class="pt-6 px-4 sm:px-6 lg:px-8">
+          <nav class="flex justify-center">
+            <ol class="space-y-6">
+              <form-step-late label="Register" />
+              <form-step-current label="Activate account" />
+              <form-step-upcoming label="Setup a workspace" />
+              <form-step-upcoming label="Shorten links!" />
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
