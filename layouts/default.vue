12c8e1029f7916b3cea59492817a49c5d0136398
Arthur Dufour <arthur@road-b-score.com>
2021-05-18 11:19:10 +0200

    fix(default-layout): min-h-screen

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div :class="getColorScheme()">
-    <div class="flex flex-col items-center bg-gray-50 dark:bg-gray-850">
+    <div class="flex flex-col items-center min-h-screen bg-gray-50 dark:bg-gray-850">
       <Navbar />
 
       <Nuxt />
